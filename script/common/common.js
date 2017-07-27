c898522b799d97a0708812140caa23ced2cce7e5
baiyu(白羽羽) <by@HOLD.FOUNDER.COM>
2017-07-27 14:56:27 +0800

    自动为xml添加id

--- script/common/common.js
+++ script/common/common.js
@@ -8,8 +8,9 @@
 
     const common  = win.Common = win.Common || {
 
-            // xml相关的基础私有变量
+            // xml相关的基础变量
             _documentId: 'data/casb15070064.xml',
+            // _documentId: 'data/introducing-texture.xml',
 
             // 公共全局模块变量
             module:{},
@@ -25,29 +26,7 @@
             // trans_abstracts:xmlDom.getElementsByTagName('trans-abstract'),
 
             html:{},
-            // 递归遍历所有xml节点，并存储有用的节点
-            getXMLNode: function (xmlElement, arr) {
 
-                //判断DOM有无子DOM
-                if (xmlElement.hasChildNodes()) {
-                    //因为xml的文本节点是单独作为元素节点的子节点出现的
-
-                    //1、找到所有叶子节点，并放入arr数组
-                    if (xmlElement.childNodes.length == 1 && xmlElement.childNodes[0].nodeType == 3) {
-                        arr.push(xmlElement);
-                    }
-                    //如果有, 遍历之
-                    for (let item of xmlElement.childNodes) {
-                        this.getXMLNode(item, arr);
-                    }
-
-                }
-                // 如果没有子元素，返回
-                else {
-                    return false;
-                }
-
-            },
         }
 })(window)
     // xml相关的基础私有变量
--- script/module/xmlFormat.js
+++ script/module/xmlFormat.js
@@ -16,15 +16,58 @@
         },
         bindEvents: function () {
             const _self = this;
-            _self.getXmlNode();
+            const _xmlDom = _self.xmlDom;
+            Common.xml.article = _self.article = _xmlDom.getElementsByTagName('article')[0];
+
+            _self.attrJson = {};
+            _self.getXMLNode(_self.article);
+            console.log(_xmlDom);
+
             _self.addAttrId();
         },
+
+        // 递归遍历所有xml节点，添加id并存储
+        getXMLNode: function (xmlElement) {
+            const _self = this;
+            const name = xmlElement.nodeName;
+            //判断DOM有无子DOM
+            if (xmlElement.hasChildNodes()) {
+                // 如果该类元素从来未出现（即未为其赋id值），或者，才开始操作xml文件，存储属性的json对象为空
+                if (!(name in _self.attrJson) || !(_self.attrJson)) {
+                    xmlElement.setAttribute('id', name + '-' + 1);
+                    _self.attrJson[name] = name + '-' + 1;
+                }
+                else {
+                    const oldId = _self.attrJson[name];
+                    const newId = _self.serialId(oldId);
+                    xmlElement.setAttribute('id', newId);
+                    // 更新json中属性值为最新（后者可以覆盖前者）
+                    _self.attrJson[name] = newId;
+                }
+                //如果有, 遍历之
+                for (let item of xmlElement.childNodes) {
+                    this.getXMLNode(item);
+                }
+
+            }
+            // 如果没有子元素，返回
+            // else {
+            //     return false;
+            // }
+        },
+        // 自动为shuxingid值+1
+        serialId: function (oldId) {
+            const length = oldId.length;
+            const num = (parseInt(oldId.charAt(length - 1))) + 1;
+            const newId = oldId.substr(0,length - 1) + num;
+            return newId;
+        },
+
         getXmlNode: function () {
             const _self = this;
             const _xmlDom = _self.xmlDom;
 
             // 得到所有的list格式的节点序列
-            Common.xml.article = _self.article = _xmlDom.getElementsByTagName('article');
             Common.xml.front = _self.front = _xmlDom.getElementsByTagName('front');
             Common.xml.article_meta = _self.article_meta = _xmlDom.getElementsByTagName('article-meta');
             Common.xml.title_group = _self.title_group = _xmlDom.getElementsByTagName('title-group');
@@ -38,7 +81,6 @@
             Common.xml.abstract = _self.abstract = _xmlDom.getElementsByTagName('abstract');
             Common.xml.trans_abstract = _self.trans_abstract = _xmlDom.getElementsByTagName('trans-abstract');
             Common.xml.p = _self.p = _xmlDom.getElementsByTagName('p');
-
 
 
         },
